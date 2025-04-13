15af748ba612f1547378121687bcc0df0afc9c0e
Thomas Müller <mimmi20@live.de>
2025-04-13 23:03:20 +0200

    rewrite to esm, update depedendencies

---
+++ prettier.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('prettier').Config} */
+const config = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  printWidth: 200,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+  singleAttributePerLine: true,
+  singleQuote: true,
+  endOfLine: 'lf',
+  semi: true,
+};
+
+export default config;
